4a2142a65eff5c8cff7ba647fbf2c91286f5b0f8
Blake <blameswalker@gmail.com>
2023-03-23 08:22:48 -0700

    Make object that stores expression values; fix 'clear' function; make addDecimalPlace

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let expression = {
+    firstOperand: '',
+    secondOperand: '',
+    operator: '',
+    firstOperandHasDecimal: false,
+    secondOperandHasDecimal: false,
+};
+expression;
+
+//Contains reference for display div
+const display = document.querySelector('#display');
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -14,45 +26,101 @@ function divide(num1, num2) {
     return num1/num2;
 }
 
-let firstNum;
-let secondNum;
-let operator;
-
-function operate(firstNum, secondNum, operator) {
-    switch (operator) {
+function operate(exp) {
+    switch (exp.operator) {
         case '+':
-            return add(firstNum, secondNum);
+            return add(exp.firstOperand, exp.secondOperand);
             break;
         case '-':
-            return subtract(firstNum, secondNum);
+            return subtract(exp.firstOperand, exp.secondOperand);
             break;
         case '*':
-            return multiply(firstNum, secondNum);
+            return multiply(exp.firstOperand, exp.secondOperand);
             break;
         case '/':
-            return divide(firstNum, secondNum);
+            return divide(exp.firstOperand, exp.secondOperand);
             break;
         default:
             return 'ERROR';
     }
 }
 
+//Get the first operand
+//Get the operator
+//Get the second number
+//Execute the expression when equals button is pressed
+//Store output value for further calculation
+//Repeat
+function getOperand() {
+    console.log(`textContent: ${this.textContent}`);
+    console.log(`type: ${typeof this.textContent}`);
+    expression;
+    if (expression.operator == '') {
+        expression.firstOperand += this.textContent;
+        console.log(`expression.firstOperand: ${expression.firstOperand}`);
+    } else {
+        expression.secondOperand += this.textContent;
+        console.log(`expression.secondOperand: ${expression.secondOperand}`);;
+    }
+    updateDisplay();
+}
+function getOperator() {
+    expression;
+    // if there is a first operand present, and no second operand present, then assign an operator value to expression.operator
+    ((expression.firstOperand != '') && (expression.secondOperand == '')) ? expression.operator = this.textContent : expression.operator = '';
+    updateDisplay();
+}
+function addDecimalPlace() {
+    expression;
+    if (expression.operator == '') {
+        if (expression.firstOperandHasDecimal == false) {
+            expression.firstOperand += '.';
+        } else {
+            return;
+        }
+    } else {
+        if (expression.secondOperandHasDecimal == false) {
+            expression.secondOperand += '.';
+        } else {
+            return;
+        }
+    }
+    updateDisplay();
+}
+
+function updateDisplay() {
+    expression;
+    display.textContent = `${expression.firstOperand} ${expression.operator} ${expression.secondOperand}`;
+}
+function clear() {
+    expression = {
+        firstOperand: '',
+        secondOperand: '',
+        operator: '',
+        firstOperandHasDecimal: false,
+        secondOperandHasDecimal: false,
+    };
+    updateDisplay();
+}
+
+function attemptCalculation() {
+    console.log(this);
+}
+
 //Contains references for ALL buttons
 const buttons = Array.from(document.querySelectorAll('button'));
 
-//Contains references for add, subtract, multiply, divide, decimal, and equals buttons ONLY
-const operators = Array.from(document.querySelectorAll('.operator'));
-
-//Contains references for number buttons ONLY
-const operands = buttons.filter(element => {
-    return !(operators.includes(element));
-});
-for (const operand of operands) {
-    operand.addEventListener('click', function updateDisplay() {
-        let currentDisplayValue = +this.innerHTML;
-        display.textContent += currentDisplayValue;
-    });
+//Adds event listeners for ALL buttons
+for (const button of buttons) {
+    if (isFinite(button.textContent)) {
+        button.addEventListener('click', getOperand);
+    } else if (button.textContent == 'Clear') {
+        button.addEventListener('click', clear);
+    } else if (button.textContent == '=') {
+        button.addEventListener('click', attemptCalculation)
+    } else if (button.textContent == '.') {
+        button.addEventListener('click', addDecimalPlace);
+    } else {
+        button.addEventListener('click', getOperator);
+    }
 }
-
-//Contains reference for display div
-const display = document.querySelector('#display');
